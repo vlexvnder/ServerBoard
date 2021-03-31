8d1389576187cd273fae233c6ae15123bc404a96
Alexander Goldberg <vlexvnder@gmail.com>
2021-03-31 23:34:10 +0000

    added configurable host ip

--- app.py
+++ app.py
@@ -4,8 +4,17 @@ from client.client import client
 
 app = Flask(__name__)
 
+SECRET_KEY = b'1$\xfbs\x88\xaf\xd3\xf7;\xb0v\x97Xe\x19\xa6'
+
 app.register_blueprint(api, url_prefix='/api')
 app.register_blueprint(client, url_prefix='/')
 
+config_file_path = 'config/config.json'
+
+with open(config_file_path) as f:
+    config = json.load(f)
+
+app_ip = config['app_ip']
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host=app_ip)
