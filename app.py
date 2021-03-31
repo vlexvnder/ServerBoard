3be4c49173504151085be5e46b128e6cf9eddd7b
Alexander Goldberg <vlexvnder@gmail.com>
2021-03-31 22:34:07 +0000

    added backend folder

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request
-from manager import compose, deleteCompose
 from apis import blueprint as api
 from client.client import client
 
 app = Flask(__name__)
+
 app.register_blueprint(api, url_prefix='/api')
 app.register_blueprint(client, url_prefix='/')
 
