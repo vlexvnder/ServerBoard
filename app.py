28c40d9a73a4e42a58cf311232c5445acfd8674d
Alexander Goldberg <vlexvnder@gmail.com>
2021-03-31 22:10:23 +0000

    added client.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from flask import Flask, request
 from manager import compose, deleteCompose
 from apis import blueprint as api
+from client import client
 
 app = Flask(__name__)
 app.register_blueprint(api, url_prefix='/api')
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app.register_blueprint(client, url_prefix='/')
 
 if __name__ == "__main__":
     app.run()
---
+++ client/client.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template
+
+client = Blueprint("client", __name__, 
+    template_folder="templates", 
+    static_folder="static", 
+    static_url_path="/client/static" 
+)
+
+@client_bp.route("/")
+def index():
+    return render_template("index.html")
