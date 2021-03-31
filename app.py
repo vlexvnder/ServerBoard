faedae59e36f16212e7cb6660d4ebc882a58e05a
Alexander Goldberg <vlexvnder@gmail.com>
2021-03-31 22:14:48 +0000

    added __init__.py file

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 from manager import compose, deleteCompose
 from apis import blueprint as api
-import client/client
+from client import client
 
 app = Flask(__name__)
 app.register_blueprint(api, url_prefix='/api')
