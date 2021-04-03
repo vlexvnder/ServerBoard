503cb849a92a447dde424406d760e6ee0b92b9ec
Alexander Goldberg <vlexvnder@gmail.com>
2021-04-03 04:38:23 +0000

    working on dashbaord

--- client/static/index.js
+++ client/static/index.js
@@ -2,8 +2,38 @@ const app = {
     delimiters: ['[[', ']]'],
     data() {
         return ({
-          hi: 'Hello, Vue!'
+          available_services : [
+              {
+                  name: "Heimdall",
+                  status: "Online",
+                  info: "A remote IDE",
+                },
+              {name: "Code-Server"},
+              {name: "app3"},
+              {name: "app4"}
+          ],
+          running_services : []
+
     })
     }
 }
+
+app.component('service',{
+    props: ['s'],
+    template: ` <div class="ui card">
+                    <div class="content">
+                        <div  class="header">
+                        [[ service.name ]]
+                        </div>
+                        <div class="meta">[[ service.status ]]</div>
+                        <div class="description">
+                        <p>
+                            [[ service.info ]]
+                        </p>
+                        </div>
+                    </div>
+
+                    
+                </div>`
+})
 Vue.createApp(app).mount('#app')
