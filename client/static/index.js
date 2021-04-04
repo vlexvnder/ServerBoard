fc09d0054038eb4f1592ad330ed710c06889c8a2
Alexander Goldberg <vlexvnder@gmail.com>
2021-04-04 04:29:11 +0000

    went back to plain html, no components

--- client/static/index.js
+++ client/static/index.js
@@ -18,22 +18,4 @@ const app = {
     }
 }
 
-app.component('service',{
-    props: ['s'],
-    template: ` <div class="ui card">
-                    <div class="content">
-                        <div  class="header">
-                        [[ service.name ]]
-                        </div>
-                        <div class="meta">[[ service.status ]]</div>
-                        <div class="description">
-                        <p>
-                            [[ service.info ]]
-                        </p>
-                        </div>
-                    </div>
-
-                    
-                </div>`
-})
 Vue.createApp(app).mount('#app')
