beb138762e9e68d65025fdbb1ccb03ea371d9f6d
Alex Goldberg <vlexvnder@gmail.com>
2021-07-18 20:32:06 -0700

    fix

--- backend/apis.py
+++ backend/apis.py
@@ -10,7 +10,7 @@ api = Api(blueprint)
 @api.route('/getServices')
 class create(Resource):
     def get(self):
-        return getOnlineServices().join(getOfflineServices)
+        return getOnlineServices() + getOfflineServices()
 
 @api.route('/create')
 class create(Resource):
